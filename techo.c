be028ef0e01f17dd7b10d3e63332028fed031fd9
fordsfords <sford@geeky-boy.com>
2020-10-25 14:02:25 -0400

    Update techo.c

--- techo.c
+++ techo.c
@@ -1,4 +1,13 @@
 /* techo.c */
+/*
+# This code and its documentation is Copyright 2020 Steven Ford, http://geeky-boy.com
+# and licensed "public domain" style under Creative Commons "CC0": http://creativecommons.org/publicdomain/zero/1.0/
+# To the extent possible under law, the contributors to this project have
+# waived all copyright and related or neighboring rights to this work.
+# In other words, you can use this code for any purpose without any
+# restrictions.  This work is published from: United States.  The project home
+# is https://github.com/fordsfords/techo
+*/
 
 #include <stdio.h>
 #include <string.h>
